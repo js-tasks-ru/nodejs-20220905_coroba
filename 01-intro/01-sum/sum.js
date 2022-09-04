7fc671faf93ca81e8715ac39222221b7d4740cf2
Roman.Korobicyn <roman.korobicyn@redsys.ru>
2022-09-04 22:23:10 +0300

    Решение задачи 01. Сумма двух чисел

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError("Неправильный тип данных. Укажите, пожалуйста, числа");
+    }
+    
+    return parseInt(a) + parseInt(b);
+
 }
 
 module.exports = sum;
